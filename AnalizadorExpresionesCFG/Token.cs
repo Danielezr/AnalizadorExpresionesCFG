650612dabc1663f5ccfdc00f0458db88a97d2607
Daniel <daniel.espzr@gmail.com>
2026-05-16 10:13:48 -0700

    Token.cs define la ficha de cada token

---
+++ AnalizadorExpresionesCFG/Token.cs
@@ -0,0 +1,21 @@
+﻿namespace AnalizadorExpresionesCFG
+{
+    public class Token
+    {
+        public string Valor { get; private set; }
+        public TipoToken Tipo { get; private set; }
+        public int Posicion { get; private set; }
+
+        public Token(string valor, TipoToken tipo, int posicion)
+        {
+            Valor = valor;
+            Tipo = tipo;
+            Posicion = posicion;
+        }
+
+        public override string ToString()
+        {
+            return "Valor: " + Valor + " | Tipo: " + Tipo + " | Posición: " + Posicion;
+        }
+    }
+}
