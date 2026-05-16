3a4f935287b4df5e8f89731833e3733c8b0f8384
Daniel <daniel.espzr@gmail.com>
2026-05-16 10:00:51 -0700

    enum TipoToken implementado para tipos de tokens

---
+++ AnalizadorExpresionesCFG/TipoToken.cs
@@ -0,0 +1,17 @@
+﻿namespace AnalizadorExpresionesCFG
+{
+
+    //enum para representar los tipos de tokens que se pueden encontrar en la expresión matemática.
+    public enum TipoToken
+    {
+        Numero,
+        OperadorSuma,
+        OperadorResta,
+        OperadorMultiplicacion,
+        OperadorDivision,
+        OperadorPotencia,
+        ParentesisIzquierdo,
+        ParentesisDerecho,
+        Fin
+    }
+}
